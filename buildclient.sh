2220721b567ddecb9440b92a18e8df7c5bbafc6c
Ernest Msugh Chia <iternenge469@gmail.com>
2026-08-21 12:13:47 +0100

    main

--- buildclient.sh
+++ buildclient.sh
@@ -2,6 +2,6 @@ cd client
 npm install
 npm run build
 cd ..
-rm -r public/assets/*
-rsync -r client/dist/* public/
+rm -r public/assets
+cp -r client/dist/* public
 mv public/index.html public/indexer.html
